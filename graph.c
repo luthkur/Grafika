dfb89e14a0bdfa8935ae80d142c6876815360a98
Deny Krisna <idpdka@yahoo.com>
2017-02-15 17:47:20 +0700

    Pecahan sudah aman

--- graph.c
+++ graph.c
@@ -827,19 +827,19 @@ void *startPlane(void *threadarg) {
 	
 	if(planeCrash==0) planeCrash=1;
     deletePolyline(&p);
-    
-    iii = 30;
+    iii = 60;
     int ii = 0;
 	int Mantul = 0;
+	deletePolyline(&plane2);
     while (ii<1000){
         
-        movePolyline(&plane1, -80, iii);
+        movePolyline(&plane1, -60, iii);
         rotatePolyline(&plane1, plane1.xp, plane1.yp, -10);
         fillPolyline(&plane1, 100,200,200,0);
         
-        movePolyline(&plane2, -40, iii);
-        rotatePolyline(&plane2, plane2.xp, plane2.yp, -10);
-        fillPolyline(&plane2, 100,200,200,0);
+        // movePolyline(&plane2, 0, iii+30);
+        // rotatePolyline(&plane2, plane2.xp, plane2.yp, -10);
+        // fillPolyline(&plane2, 100,200,200,0);
         
 		if(Mantul==0) {
 			movePolyline(&plane3, 30, iii);
@@ -847,12 +847,12 @@ void *startPlane(void *threadarg) {
 				Mantul=1;
 			}
 		} else {
-			movePolyline(&plane3, 40, -iii);
+			movePolyline(&plane3, 80, -iii);
 		}
 		rotatePolyline(&plane3, plane3.xp, plane3.yp, 10);
         fillPolyline(&plane3, 100,200,200,0);
         
-		movePolyline(&plane4, 60, iii);
+		movePolyline(&plane4, 120, iii);
         rotatePolyline(&plane4, plane4.xp, plane4.yp, 10);
 		fillPolyline(&plane4, 100,200,200,0);
 		
